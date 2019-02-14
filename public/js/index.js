084e9fa0e497563153bd53ea3e86f6acc6cb89e7
Kelsey <kelsgainer@gmail.com>
2019-02-13 20:10:47 -0700

    button update

--- public/js/index.js
+++ public/js/index.js
@@ -20,5 +20,12 @@ function hide_sidebar() {
 // navbar end
 
 
-// grid
+// heart button
+
+$(function() {
+  $('.button-like')
+    .bind('click', function(event) {
+      $(".button-like").toggleClass("liked");
+    })
+});
 
